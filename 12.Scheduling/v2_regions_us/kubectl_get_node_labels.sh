055f530acb19b276e3a551042cf9592ee115c2d0
Michael Bright <1880109+mjbright@users.noreply.github.com>
2024-10-18 11:03:27 +0000

    Updated demo to use 'real' regions/zones, AND split of resources to prevent race condition

--- 12.Scheduling/v2_regions_us/kubectl_get_node_labels.sh
+++ 12.Scheduling/v2_regions_us/kubectl_get_node_labels.sh
@@ -1,6 +1,9 @@
 
 kubectl get nodes --show-labels |
-    sed -e 's/[A-z,\-]*.kubernetes.io\/[^,]*//g'  -e 's/ ,/ /'
+    sed -e 's/[A-z,\-]*.kubernetes.io\/[^,]*//g' \
+        -e 's/system=[^,]*,//g' \
+        -e 's/ ,/ /'
+
 
 
 
--- 12.Scheduling/v2_regions_us/schedule_set_labels.sh
+++ 12.Scheduling/v2_regions_us/schedule_set_labels.sh
@@ -1,14 +1,14 @@
 
 # Region us-east:
-  kubectl label node cp      region=us-east
-  kubectl label node cp      zone="us-east-1"
+  kubectl label node cp      region=us-east --overwrite
+  kubectl label node cp      zone="us-east-1" --overwrite
 
-  kubectl label node worker  region=us-east
-  kubectl label node worker  zone="us-east-2"
+  kubectl label node worker  region=us-east --overwrite
+  kubectl label node worker  zone="us-east-2" --overwrite
 
 # Region us-west:
-  kubectl label node cp2 region=us-west
-  kubectl label node cp2 zone="us-west-1"
+  kubectl label node secondcp region=us-west --overwrite
+  kubectl label node secondcp zone="us-west-1" --overwrite
 
-  kubectl label node cp3 region=us-west
-  kubectl label node cp3 zone="us-west-2"
+  kubectl label node thirdcp region=us-west --overwrite
+  kubectl label node thirdcp zone="us-west-2" --overwrite
